0a4c6c9461df0f61c1a4cc47dc057fdd787ff1f1
Dash Eclipse <dash@ezup.dev>
2020-07-16 16:52:22 +0000

    slock: add config.h

---
+++ srcpkgs/slock/files/config.h
@@ -0,0 +1,25 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#2E3440",   /* during input */
+	[FAILED] = "#BF616A",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#D8DEE9";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-xos4-terminus-medium-r-normal--18-180-72-72-c-100-iso10646-1";
