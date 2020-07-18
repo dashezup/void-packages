f24e3e9c3336639a3a2057451da9eb9eb90f0377
Dash Eclipse <dash@ezup.dev>
2020-07-18 23:17:14 +0000

    Update ezup/

--- ezup/build-pkg.sh
+++ ezup/build-pkg.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 for pkg in "$@"; do
+	echo "\n\n## Cleaning...\n"
+	./xbps-src clean
+
 	echo "\n\n## Building $pkg (x86_64-musl)...\n"
 	./xbps-src -r musl pkg $pkg
+
 	echo "\n\n## Building $pkg (armv7l-musl)...\n"
 	./xbps-src -a armv7l-musl -r musl pkg $pkg
+
 	echo "\n\n## Building $pkg (aarch64)...\n"
 	./xbps-src -a aarch64-musl -r aarch64 pkg $pkg
 done
