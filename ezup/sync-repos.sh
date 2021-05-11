9875bfe10fac197359b748e7d65722b7d7f8749c
Dash Eclipse <dashezup@disroot.org>
2021-05-11 07:03:12 +0000

    ezup/sync-repos.sh: fix remote file path of xupdategit

--- ezup/sync-repos.sh
+++ ezup/sync-repos.sh
@@ -7,4 +7,4 @@ echo '\n\n### Syncing musl/...\n'
 rsync -rv --delete hostdir/binpkgs/ezupdev/ ezup:~/ezup/repo/voidlinux/current/
 
 echo '\n\n### Updating git based xlocate database...\n'
-ssh ezup ~/ezup/repo/voidlinux/xlocate/xupdategit
+ssh ezup ezup/repo/voidlinux/xlocate/xupdategit
